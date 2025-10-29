b645110c1a80c9e11c0071441d26ebba8479c602
Jesús <jesus.lab.tech@gmail.com>
2025-10-29 11:58:44 +0100

    Dev sync to main in one commit
    * container wrapper in the layout
    
    * General styles conf
    
    * test h1
    
    * tailwind conf
    
    * hero background image
    
    * lucide-astro icons
    
    * Alias conf in typescript
    
    * renamed
    
    * deleted tailwind.js
    
    * new alias @/*
    
    * feat: basic button component
    
    * fix: the correct way to apply the dark style
    
    * feat: new layout style
    
    * feat: basic header component. It is not responsive complete
    
    * feat: basic hero component
    
    * feat: first index placeholder
    
    * fix: correct colors
    
    * fix: correct colors and cursos pointer hover added
    
    * fix: correct colors and cursor pointer hover added
    
    * feat: Card component's components
    
    * feat: card component
    
    * feat: Badge Component
    
    * fix: id of the experience section
    
    * feat: main experience component
    
    * add projects ideas to gitignore
    
    * feat: Projects component
    
    * feat: Added projects section
    
    * fix:  no need of a section inside the component
    
    * feat: Content Collection config
    
    * Add a test  markdown file
    
    * feat: New page that render every article
    
    * feat: new Posts component added
    
    * feat: new section posts added
    
    * feat: ClientRouter support, aka ViewTransitions
    
    * feat: new transition for title and image. Image wrapped by anchor too
    
    * feat: view transition linked
    
    * removed: console.log
    
    * Rename test post
    
    * ignoring other test posts
    
    * fix: display of the posts in large screens
    
    * fix: span color changed to improve visibility
    
    * feat: readded the logo and some adjusts of the mobile menu
    
    * fix: achievements
    
    * feat: Contact component
    
    * Added Contact component
    
    * chore: new format for background image and removed comments
    
    * feat: Footer component
    
    * feat: added fotter component
    
    * chore: new footer
    
    * chore: icons colors
    
    * chore: some mobile display adjustments
    
    * chore: add todo file
    
    * chore: new hero description
    
    * feat: new header for post page in order to go back to index page
    
    * fix: Bug in the init of the header's blur script while scrolling
    
    * feat: SEO
    
    * chore: remove unnecessary resources

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        lang: z.enum(['en', 'es']).default('es'),
+        author: z.string().default('Jesús Merlo'),
+        email: z.string().email().default('jesus.lab.tech@gmail.com'),
+        date: z.string().refine((date) => !isNaN(Date.parse(date)), {
+            message: 'Invalid date format',
+        }),
+        description: z.string(),
+        tags: z.array(z.string()).optional(),
+        draft: z.boolean().optional().default(false),
+        coverImage: z.string().url().optional(),
+    }), 
+});
+
+export const collections = { posts };
